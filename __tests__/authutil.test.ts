5273d0df9c603edc4284ac8402cf650b4f1f6686
Danny McCormick <damccorm@microsoft.com>
2019-08-06 18:29:44 -0400

    Add auth

---
+++ __tests__/authutil.test.ts
@@ -0,0 +1,62 @@
+import io = require('@actions/io');
+import fs = require('fs');
+import path = require('path');
+
+const tempDir = path.join(
+  __dirname,
+  'runner',
+  path.join(
+    Math.random()
+      .toString(36)
+      .substring(7)
+  ),
+  'temp'
+);
+
+const rcFile = path.join(tempDir, '.npmrc');
+
+process.env['GITHUB_REPOSITORY'] = 'owner/repo';
+process.env['RUNNER_TEMP'] = tempDir;
+import * as auth from '../src/authutil';
+
+describe('installer tests', () => {
+  beforeAll(async () => {
+    await io.rmRF(tempDir);
+    await io.mkdirP(tempDir);
+  }, 100000);
+
+  beforeEach(() => {
+    if (fs.existsSync(rcFile)) {
+      fs.unlinkSync(rcFile);
+    }
+    process.env['INPUT_SCOPE'] = '';
+  });
+
+  it('Sets up npmrc for npmjs', async () => {
+    await auth.configAuthentication('https://registry.npmjs.org/');
+    expect(fs.existsSync(rcFile)).toBe(true);
+    expect(fs.readFileSync(rcFile, {encoding: 'utf8'})).toMatchSnapshot();
+  });
+
+  it('Appends trailing slash to registry', async () => {
+    await auth.configAuthentication('https://registry.npmjs.org');
+
+    expect(fs.existsSync(rcFile)).toBe(true);
+    expect(fs.readFileSync(rcFile, {encoding: 'utf8'})).toMatchSnapshot();
+  });
+
+  it('Configures scoped npm registries', async () => {
+    process.env['INPUT_SCOPE'] = 'myScope';
+    await auth.configAuthentication('https://registry.npmjs.org');
+
+    expect(fs.existsSync(rcFile)).toBe(true);
+    expect(fs.readFileSync(rcFile, {encoding: 'utf8'})).toMatchSnapshot();
+  });
+
+  it('Automatically configures GPR scope', async () => {
+    await auth.configAuthentication('npm.pkg.github.com');
+
+    expect(fs.existsSync(rcFile)).toBe(true);
+    expect(fs.readFileSync(rcFile, {encoding: 'utf8'})).toMatchSnapshot();
+  });
+});
---
+++ src/authutil.ts
@@ -0,0 +1,50 @@
+import * as fs from 'fs';
+import * as os from 'os';
+import * as path from 'path';
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+
+export function configAuthentication(registryUrl: string) {
+  const npmrc: string = path.resolve(
+    process.env['RUNNER_TEMP'] || process.cwd(),
+    '.npmrc'
+  );
+  if (!registryUrl.endsWith('/')) {
+    registryUrl += '/';
+  }
+
+  writeRegistryToFile(registryUrl, npmrc);
+}
+
+function writeRegistryToFile(registryUrl: string, fileLocation: string) {
+  let scope: string = core.getInput('scope');
+  if (!scope && registryUrl.indexOf('npm.pkg.github.com') > -1) {
+    scope = github.context.repo.owner;
+  }
+  if (scope && scope[0] != '@') {
+    scope = '@' + scope;
+  }
+
+  core.debug(`Setting auth in ${fileLocation}`);
+  let newContents: string = '';
+  if (fs.existsSync(fileLocation)) {
+    const curContents: string = fs.readFileSync(fileLocation, 'utf8');
+    curContents.split(os.EOL).forEach((line: string) => {
+      // Add current contents unless they are setting the registry
+      if (!line.toLowerCase().startsWith('registry')) {
+        newContents += line + os.EOL;
+      }
+    });
+  }
+  // Remove http: or https: from front of registry.
+  const authString: string =
+    registryUrl.replace(/(^\w+:|^)/, '') + ':_authToken=${NODE_AUTH_TOKEN}';
+  const registryString: string = scope
+    ? `${scope}:registry=${registryUrl}`
+    : `registry=${registryUrl}`;
+  newContents += `${authString}${os.EOL}${registryString}`;
+  fs.writeFileSync(fileLocation, newContents);
+  core.exportVariable('NPM_CONFIG_USERCONFIG', fileLocation);
+  // Export empty node_auth_token so npm doesn't complain about not being able to find it
+  core.exportVariable('NODE_AUTH_TOKEN', 'XXXXX-XXXXX-XXXXX-XXXXX');
+}
