e9ee86ba96bbcb4528f2ac3494e508ef3f67474e
eric sciple <ericsciple@users.noreply.github.com>
2020-01-24 17:18:44 -0500

    .

--- __tests__/verify-node.sh
+++ __tests__/verify-node.sh
@@ -5,21 +5,9 @@ if [ -z "$1" ]; then
   exit 1
 fi
 
-if [ -z "$2" ]; then
-  echo "Must supply npm version argument"
-  exit 1
-fi
-
 node_version="$(node --version)"
 echo "Found node version '$node_version'"
 if [ -z "$(echo $node_version | grep v$1)" ]; then
-  echo "Unexpected version"
-  exit 1
-fi
-
-npm_version="$(npm --version)"
-echo "Found npm version '$npm_version'"
-if [ -z "$(echo $npm_version | grep v$2)" ]; then
   echo "Unexpected version"
   exit 1
 fi
