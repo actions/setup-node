1c0119c056919c9913db0d2c11e75e370a7e225c
eric sciple <ericsciple@users.noreply.github.com>
2020-05-18 11:37:12 -0400

    .

--- dist/index.js
+++ dist/index.js
@@ -10943,7 +10943,7 @@ function downloadTool(url, dest, auth) {
                 }
             }
             // Otherwise retry
-            return true;
+            return false;
         });
     });
 }
@@ -10965,7 +10965,7 @@ function downloadToolAttempt(url, dest, auth) {
             };
         }
         const response = yield http.get(url, headers);
-        throw new HTTPError(429);
+        throw new HTTPError(500);
         if (response.message.statusCode !== 200) {
             const err = new HTTPError(response.message.statusCode);
             core.debug(`Failed to download from "${url}". Code(${response.message.statusCode}) Message(${response.message.statusMessage})`);
