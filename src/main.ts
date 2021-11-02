6382f41129f598a4480a2e1711298db096adf3f9
Hargun Kaur <56452820+hkaur008@users.noreply.github.com>
2021-11-02 13:34:36 +0000

    checks implemented for node version file

--- src/main.ts
+++ src/main.ts
@@ -74,6 +74,17 @@ function isGhes(): boolean {
 }
 
 function resolveVersionInput(): string {
+ 
+  if(!(core.getInput('node-version') || core.getInput('version')))
+  {
+    core.error('No specified file exists')
+  }
+
+  if(core.getInput('node-version') && core.getInput('version'))
+  {
+    core.warning('Both version and node-version-file are specified')
+  }
+
   let version = core.getInput('node-version') || core.getInput('version');
   if (version) {
     return version;
