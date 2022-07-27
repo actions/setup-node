5b579f16381b804435968b7cb2131868c08c0350
Jef LeCompte <jeffreylec@gmail.com>
2022-07-27 14:31:06 -0700

    refactor: check volta first, then engine

--- src/installer.ts
+++ src/installer.ts
@@ -502,10 +502,9 @@ export function parseNodeVersionFile(contents: string): string {
 
   if (!nodeVersion) {
     try {
-      // Try parsing the file as an NPM `package.json`
-      // file.
-      nodeVersion = JSON.parse(contents).engines?.node;
-      if (!nodeVersion) JSON.parse(contents).volta?.node;
+      // Try parsing the file as an NPM `package.json` file.
+      nodeVersion = JSON.parse(contents).volta?.node;
+      if (!nodeVersion)nodeVersion = JSON.parse(contents).engines?.node;
       if (!nodeVersion) throw new Error();
     } catch (err) {
       // In the case of an unknown format,
