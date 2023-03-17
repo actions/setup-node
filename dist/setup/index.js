785951b47f3bc603854eb9f2518b81ac14f00a7c
adilhusain-s <Adilhusain.Shaikh@ibm.com>
2023-03-17 17:34:19 +0530

    adding support for linux-ppc64le

--- dist/setup/index.js
+++ dist/setup/index.js
@@ -73562,6 +73562,8 @@ class BaseDistribution {
         switch (arch) {
             case 'arm':
                 return 'armv7l';
+            case 'ppc64':
+                return 'ppc64le';
             default:
                 return arch;
         }
