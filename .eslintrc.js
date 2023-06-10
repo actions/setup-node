e1f19ff0af147abf907fd157a152d646cce52647
Anglemmj7 <angelennab@v2-mining.com>
2023-06-10 09:56:49 +0100

    Revert "Remove implicit dependencies"

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,7 @@ module.exports = {
     'eslint-config-prettier'
   ],
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'eslint-plugin-node', 'eslint-plugin-jest'],
+  plugins: ['@typescript-eslint', 'eslint-plugin-jest'],
   rules: {
     '@typescript-eslint/no-require-imports': 'error',
     '@typescript-eslint/no-non-null-assertion': 'off',
@@ -28,8 +28,7 @@ module.exports = {
       }
     ],
     'no-control-regex': 'off',
-    'no-constant-condition': ['error', {checkLoops: false}],
-    'node/no-extraneous-import': 'error'
+    'no-constant-condition': ['error', {checkLoops: false}]
   },
   overrides: [
     {
