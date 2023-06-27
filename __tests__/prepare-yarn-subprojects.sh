e33196f7422957bea03ed53f6fbb155025ffc7b8
Sergey Dolin <dsame@github.com>
2023-06-27 13:07:43 +0200

    Do not ivalidate the cache entirely on lock file change (#744)
    * Do not ivalidate the cache entirely on yarn3 lock file change
    
    * Use cache prefix if all sub-projects are yarn managed
    
    * Rename functions & add e2e tests

--- __tests__/prepare-yarn-subprojects.sh
+++ __tests__/prepare-yarn-subprojects.sh
@@ -32,10 +32,20 @@ cat <<EOT >package.json
 EOT
 yarn set version 3.5.1
 yarn install
+if [ x$1 = 'xglobal' ];then
+  echo enableGlobalCache
+  echo 'enableGlobalCache: true' >> .yarnrc.yml
+fi
 
-echo "create yarn1 project in the root"
 cd ..
-cat <<EOT >package.json
+if [ x$1 != 'xkeepcache' -a x$2 != 'xkeepcache' ]; then
+  rm -rf sub2/.yarn/cache
+  rm -rf sub3/.yarn/cache
+fi
+
+if [ x$1 = 'xyarn1' ];then
+  echo "create yarn1 project in the root"
+  cat <<EOT >package.json
 {
   "name": "subproject",
   "dependencies": {
@@ -44,5 +54,6 @@ cat <<EOT >package.json
   }
 }
 EOT
-yarn set version 1.22.19
-yarn install
+  yarn set version 1.22.19
+  yarn install
+fi
